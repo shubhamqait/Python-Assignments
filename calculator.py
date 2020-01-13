3862a5664b415375d5241ece63caaf5f4d56e760
Ankit Kumar Singh <ankitkumarsingh@qainfotech.com>
2020-01-13 09:51:43 +0530

    First Review Pass

--- calculator.py
+++ calculator.py
@@ -1,23 +1,25 @@
 def run_calculator():
+    #ankitksr: Try using boolean flag instead. 
     i = int(input("Press '1' For use calculator again else press '0' for Exit ->"))
     if i == 1:
         run_again()
     elif i == 0:
         exit()
 
-my_list = []
+my_list = []   #ankitksr: Variable declaration/initialization should always be at the top of the module.
 def enter():
     try: 
         print("\n")
         print("Note: You Enter Unlimited Numbers But After entering all numbers type 'stop' for result")
         print("Enter first number")
         while True: 
-            my_list.append(int(input())) 
+            my_list.append(int(input()))   # Give print prompt alongwith 'input'.
             print("Enter another number")
             
     except: 
         pass
 
+#ankitksr: IMPROPER USE OF A DECORATOR
 def add(func):
     def wrap():
         func()
