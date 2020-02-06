1d5e5f5277bb2a091354f09e1b1236aa25dbdac4
Ankit Kumar Singh <ankitkumarsingh@qainfotech.com>
2020-02-06 12:01:20 +0530

    Review: First pass

--- filehandling.py
+++ filehandling.py
@@ -1,15 +1,19 @@
+# ankitksr: THE SCRIPT DOES NOT RUN.
+
 import os
 import shutil
+# ankitksr: Do not use hard-coded folder paths. Instead, use relative paths.
 entries = os.listdir('/home/shubham/Desktop/Folder')
 filescreate=int(input("Enter how many files you want to create"))
+# ankitksr: Do not assume that the files in the folder would be named as per your preferred naming scheme. Assume random file names.
 for i in range(filescreate):
     f=open(str(i)+".txt","w+")
 
 for entry in entries:
-    print(entry)
+    print(entry)   # ankitksr: What is the purpose of this print statement?
 filedir=int(input("Enter how many files you want to move in group"))
 d=int(filescreate/filedir)
-if a%filedir==0:
+if a%filedir==0:   # ankitksr: What is 'a' here?
     pass
 else:
     d=d+1
@@ -21,6 +25,7 @@ for j in range(d):
         os.mkdir(str(j))
 e=filedir
 y=0
+# ankitksr: Unable to understand the logic used below.
 for x in range(d):
     for i in range(filedir):
         if y<filescreate: 
