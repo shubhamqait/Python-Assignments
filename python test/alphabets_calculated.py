697699c6934e6db9614e23dcbb04189a117d4824
shubhamqait <shubham2@qainfotech.com>
2020-02-28 16:31:45 +0530

    Five question solution

---
+++ python test/alphabets_calculated.py
@@ -0,0 +1,24 @@
+import requests
+import json
+username = 'shubhamgupta'
+password = 'qainfotech'
+
+url = "http://10.0.10.198:8000/api/v1/challenges/1/?format=json"
+
+response = requests.get(url, auth = (username,password))    
+show = response.json()
+data1 = show.get('sample_input')
+data2 = data1.get('text')  
+count = 0
+for x in data2:
+    if x != ' ':
+        count = count+1
+data3 = {
+    'candidate':3,
+    'challenge':1
+}
+url1 = "http://10.0.10.198:8000/api/v1/testsessions/"
+check = requests.post(url,data = data3,auth = (username,password))
+
+print(check.content)
+#print("The number of alphabets in string are : " + str(count))
